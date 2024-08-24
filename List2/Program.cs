cc19b71972b5d4905354568f8b7bff4ea4e8914f
asus <asus@DESKTOP-OTTG4TB>
2024-08-24 20:11:55 +0300

    Add project files.

---
+++ List2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<String> coffe = new List<String>(); //Önce bir liste oluşturuyoruz.
+
+        // Kullanıcıdan 5 adet kahve ismi alınacağı için for döngüsü kullanıyoruz.
+
+        for (int i = 0; i<5; i++)
+        {
+            Console.WriteLine("Kahve ismini giriniz: ");
+            string kahve = Console.ReadLine();
+            coffe.Add(kahve);
+        }
+        Console.WriteLine("Kahve isimleri:");
+        foreach (string kahve in coffe)
+        {
+            Console.WriteLine(kahve);
+
+        }
+    }
+}
